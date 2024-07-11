82b5a7830802269b3a4ff56354df621d6c6656a2
PhoopPeep <christina_c@cmu.ac.th>
2024-07-12 01:50:32 +0700

    add the axios
    add the axios and update the EveListView.vue

---
+++ 331-intro-to-vite/src/views/EventListView.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue';
+import Event from '@/types/Event';
+import CategoryOrganizer from '@/components/CategoryOrganizer.vue';
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import { error } from 'console';
+
+const events = ref<Event[]>(null)
+
+onMounted(() => {
+  axios
+  .get('https://my-json-server.typicode.com/PhoopPeep/331-lab02/events')
+  .then((response) => {
+    console.log(response.data)
+  })
+  .catch((error) => {
+    console.error('There was an error!', error)
+  })
+})
+</script>
+
+<template>
+  <div class="events">
+    <h1>Events For Good</h1>
+    <!-- new element -->
+    <EventCard v-for="event in events" key="event.id" :event="event" />
+  </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
